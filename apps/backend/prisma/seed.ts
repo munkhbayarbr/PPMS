fa03654db2488185946e2844fdcccca41ecb40ce
munkhbayar byambadelger <muba@munkhbayars-Mac-mini.local>
2025-09-29 17:47:49 +0800

    fix

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -14,39 +14,39 @@
 // // }
 // // main().finally(() => prisma.$disconnect());
 
-// import { PrismaClient, Role } from '@prisma/client';
-// import * as bcrypt from 'bcrypt';
+import { PrismaClient, Role } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// async function main() {
-//   // Default admin credentials
-//   const email = 'admin@gmail.com';
-//   const plainPassword = 'admin123';
-//   const password = await bcrypt.hash(plainPassword, 12);
+async function main() {
+  // Default admin credentials
+  const email = 'admin@gmail.com';
+  const plainPassword = 'admin123';
+  const password = await bcrypt.hash(plainPassword, 12);
 
-//   // Create or ensure an ADMIN user exists
-//   const admin = await prisma.user.upsert({
-//     where: { email },
-//     update: {},
-//     create: {
-//       email,
-//       password,
-//       name: 'Admin',
-//     },
-//   });
+  // Create or ensure an ADMIN user exists
+  const admin = await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      email,
+      password,
+      name: 'Admin',
+    },
+  });
 
-//   console.log('✅ Seeded user:', {
-//     id: admin.id,
-//     email: admin.email,
-//   });
-// }
+  console.log('✅ Seeded user:', {
+    id: admin.id,
+    email: admin.email,
+  });
+}
 
-// main()
-//   .catch((e) => {
-//     console.error('❌ Seed error:', e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+main()
+  .catch((e) => {
+    console.error('❌ Seed error:', e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
